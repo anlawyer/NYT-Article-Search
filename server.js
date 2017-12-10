2217ed5f447be0ecf452b5454e8a0e86ef8c3414
Alison Lawyer <alison@Alisons-MacBook-Air.local>
2017-12-10 12:51:20 -0800

    production build help

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 // Add routes, both API and view
 app.use(routes);
 
